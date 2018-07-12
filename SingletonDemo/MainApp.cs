39b067281fb55a6240440cc55ccf35693d03fc92
Alper Göçen <alpergocen35@gmail.com>
2018-07-12 23:31:15 +0300

    All the things we have done in Singleton class
    Singleton class demo code

---
+++ SingletonDemo/MainApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.Get_Instance();
+            Singleton s2 = Singleton.Get_Instance();
+
+
+            if (s1.Equals(s2))
+                Console.WriteLine("The instances are the same!!");
+            else
+                Console.WriteLine("The instances are different");
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SingletonDemo/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+
+     sealed class Singleton
+    {
+        private static Singleton _instance = null;
+
+        private Singleton()
+        {
+
+        }
+
+        public  static Singleton Get_Instance()
+        {
+
+            if (_instance == null)
+                _instance = new Singleton();
+
+            return _instance;
+
+        }
+        
+    }
+}
